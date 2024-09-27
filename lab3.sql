24b54bab19e46ac35776e94119eb7d3ab8b0887d
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-09-27 16:41:59 +0300

    Update lab3.sql

--- lab3.sql
+++ lab3.sql
@@ -1,29 +1,13 @@
-SELECT DISTINCT
-    regexp_replace(naimen, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБО[ЮЛ]{2}|НОТАРИУС|РОССИЙСКИЙ)\s*', '') AS справочник
-FROM
-    uch_vse
-WHERE
-    REGEXP_LIKE ( upper(naimen),
-                  '^(\w+\s){2,3}\w+$' )
-    AND REGEXP_LIKE ( upper(naimen),
-                      '[\s ]?\w*(ОВИЧ|ЕВИЧ|ИЧ|ОВНА|ЕВНА|ИЧНА|ИНЧНА|ОВ|ЕВ|ИН|ОВА|ЕВА|ИНА)$' )
-    AND regexp_count(regexp_replace(naimen, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБО[ЮЛ]{2}|НОТАРИУС|РОССИЙСКИЙ)\s*', ''),
-                     '[  \s]+') = 2;
-
-SELECT DISTINCT
-    regexp_substr(regexp_replace(naimen, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБОЮЛ|ПБОЛЮ||НОТАРИУС)\s*', ''),
-                  '\w+',
-                  1,
-                  2) AS ИМЕНА
-FROM
-    uch_vse
-WHERE
-    REGEXP_LIKE ( upper(naimen),
-                  '^(\w+\s){2,3}\w+$' )
-    AND REGEXP_LIKE ( upper(naimen),
-                      '[\s ]?\w*(ОВИЧ|ЕВИЧ|ИЧ|ОВНА|ЕВНА|ИЧНА|ИНЧНА|ОВ|ЕВ|ИН|ОВА|ЕВА|ИНА)$' )
-    AND regexp_count(regexp_replace(naimen, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБОЮЛ|ПБОЛЮ||НОТАРИУС)\s*', ''),
-                     '\s+') <= 2;
+    select distinct REGEXP_REPLACE(NAIMEN, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБО[ЮЛ]{2}|НОТАРИУС|РОССИЙСКИЙ)\s*', '') AS Справочник from uch_vse
+    where REGEXP_LIKE(upper(naimen), '^(\w+\s){2,3}\w+$')
+    AND REGEXP_LIKE(upper(naimen), '[\s ]?\w*(ОВИЧ|ЕВИЧ|ИЧ|ОВНА|ЕВНА|ИЧНА|ИНЧНА|ОВ|ЕВ|ИН|ОВА|ЕВА|ИНА)$')
+    AND REGEXP_COUNT(REGEXP_REPLACE(NAIMEN, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБО[ЮЛ]{2}|НОТАРИУС|РОССИЙСКИЙ)\s*', ''), '[  \s]+') = 2;
+    
+SELECT DISTINCT REGEXP_SUBSTR(REGEXP_REPLACE(NAIMEN, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБОЮЛ|ПБОЛЮ||НОТАРИУС)\s*', ''), '\w+', 1, 2) AS ИМЕНА
+FROM uch_vse
+WHERE REGEXP_LIKE(upper(naimen), '^(\w+\s){2,3}\w+$')
+AND REGEXP_LIKE(upper(naimen), '[\s ]?\w*(ОВИЧ|ЕВИЧ|ИЧ|ОВНА|ЕВНА|ИЧНА|ИНЧНА|ОВ|ЕВ|ИН|ОВА|ЕВА|ИНА)$')
+AND REGEXP_COUNT(REGEXP_REPLACE(NAIMEN, '^.*?([ИЧП]{2,3}|ПРЕДПРИНИМАТЕЛЬ|ПБОЮЛ|ПБОЛЮ||НОТАРИУС)\s*', ''), '\s+') <= 2;
 
 SELECT naimen, 
        REGEXP_SUBSTR(upper(naimen), '\s([ГСCП]|ПГ[ТT]|П\.Г\.[TТ])[ \s\.]{1,2}[А-Я]+(\s|$)') AS REGEXP
