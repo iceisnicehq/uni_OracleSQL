6a122ad291757abf08b4bf359ecf36f1b60d178d
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-10-16 15:09:24 +0300

    lab4_fin

--- lab4.sql
+++ lab4.sql
@@ -59,8 +59,8 @@ CREATE TABLE Avtomobil (
     Proizvoditel VARCHAR2(100) NOT NULL,
     data_vypuska DATE NOT NULL,
     Vladelec_FIO VARCHAR2(100) NOT NULL,
-    CONSTRAINT pk_avtomobil PRIMARY KEY (Nomer_avtomobilia)
-    -- CONSTRAINT fk_avto_vladelec FOREIGN KEY (Vladelec_FIO) REFERENCES Klient(FIO)
+    CONSTRAINT pk_avtomobil PRIMARY KEY (Nomer_avtomobilia),
+    -- CONSTRAINT fk_avto_vladelec FOREIGN KEY (Vladelec_FIO) REFERENCES Avtomobil(Proizvoditel)
 );
 
 CREATE TABLE Uslugi (
@@ -79,9 +79,10 @@ CREATE TABLE Zakaz_klienta (
     Nomer_uslugi NUMBER NOT NULL,
     Rabotnik VARCHAR2(10) NOT NULL,
     CONSTRAINT pk_zakaz_klienta PRIMARY KEY (Nomer_avtomobilia, Nomer_uslugi, Rabotnik),
-    CONSTRAINT fk_zakaz_avtomobil FOREIGN KEY (Nomer_avtomobilia) REFERENCES Avtomobil(Nomer_avtomobilia), -- ON UPDATE CASCADE
+    CONSTRAINT fk_zakaz_avtomobil FOREIGN KEY (Nomer_avtomobilia) REFERENCES Avtomobil(Nomer_avtomobilia), 
     CONSTRAINT fk_zakaz_uslugi FOREIGN KEY (Nomer_uslugi) REFERENCES Uslugi(Nomer_uslugi),
-    CONSTRAINT fk_zakaz_rabotnik FOREIGN KEY (Rabotnik) REFERENCES Rabotniki(Seria_i_nomer_pasporta)
+    CONSTRAINT fk_zakaz_rabotnik FOREIGN KEY (Rabotnik) REFERENCES Rabotniki(Seria_i_nomer_pasporta),
+    -- constraint fk_itself FOREIGN KEY (
 );
 ----1111----
 CREATE TABLE Service_History (
@@ -89,13 +90,41 @@ CREATE TABLE Service_History (
     Nomer_uslugi NUMBER NOT NULL,
     Data_servisa DATE NOT NULL,
     Stoimost_servisa NUMBER NOT NULL,
-    Opisanie_servisa VARCHAR2(200) NOT NULL,
+    Opisanie_servisa VARCHAR2(1024) NOT NULL,
     CONSTRAINT pk_service_history PRIMARY KEY (Nomer_avtomobilia, Nomer_uslugi, Data_servisa),
     CONSTRAINT fk_service_history_avtomobil FOREIGN KEY (Nomer_avtomobilia) REFERENCES Avtomobil(Nomer_avtomobilia),
     CONSTRAINT fk_service_history_uslugi FOREIGN KEY (Nomer_uslugi) REFERENCES Uslugi(Nomer_uslugi)
 );
-----
 
+----
+alter table Klient Rename to Client 
+-- таблицу доставки автомобилей
+CREATE TABLE car_delivery (
+    Delivery_ID NUMBER,
+    Nomer_avtomobilia VARCHAR2(10) NOT NULL,
+    Brand VARCHAR2(50) NOT NULL,
+    Delivery_date DATE NOT NULL,
+    Recipient_telefon CHAR(10) NOT NULL,
+    CONSTRAINT pk_car_delivery PRIMARY KEY (Delivery_ID),
+    CONSTRAINT fk_car_delivery_avtomobil FOREIGN KEY (Nomer_avtomobilia) REFERENCES Avtomobil(Nomer_avtomobilia),
+    CONSTRAINT fk_car_delivery_telefon FOREIGN KEY (Recipient_telefon) REFERENCES Client(Telefon)
+);
+SELECT TABLE_NAME, COMMENTS 
+FROM ALL_TAB_COMMENTS 
+WHERE TABLE_NAME = 'CAR_DELIVERY';
+
+SELECT TABLE_NAME, COLUMN_NAME, COMMENTS 
+FROM ALL_COL_COMMENTS 
+WHERE TABLE_NAME = 'CAR_DELIVERY';
+
+SeLECT * from car_delivery
+
+COMMENT ON TABLE car_delivery IS 'Таблица для хранения информации о доставке автомобилей';
+COMMENT ON COLUMN car_delivery.Delivery_ID IS 'Уникальный идентификатор доставки';
+COMMENT ON COLUMN car_delivery.Nomer_avtomobilia IS 'Номер автомобиля, который доставляется';
+COMMENT ON COLUMN car_delivery.Brand IS 'Марка автомобиля';
+COMMENT ON COLUMN car_delivery.Delivery_date IS 'Дата доставки автомобиля';
+COMMENT ON COLUMN car_delivery.Recipient_telefon IS 'Телефон получателя автомобиля';
 ----2222----
 ALTER TABLE Uslugi
 DROP CONSTRAINT fk_uslugi_oborud;
@@ -109,3 +138,4 @@ MODIFY Oborudovanie VARCHAR2(30);
 ALTER TABLE Uslugi
 ADD CONSTRAINT fk_uslugi_oborud FOREIGN KEY (Oborudovanie) REFERENCES Oborudovanie(Seriynyy_nomer);
 ----
+
