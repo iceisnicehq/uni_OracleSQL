185ec3a5b2c679e968ff9b1682478fe4976b7496
Sab <66420569+iceisnicehq@users.noreply.github.com>
2024-11-13 10:39:48 +0300

    Update lab6.sql

--- lab6.sql
+++ lab6.sql
@@ -8,10 +8,11 @@ GRANT SELECT ON sh.test_operation, sh.test_uchast TO Sabirov_DS;
 
 -- under user
 CREATE TABLE sh.operations AS 
-  SELECT *
-  FROM sh.test_operation
-  WHERE REGEXP_LIKE(naimen, '[З3][AА][OО]|[з3][аa][кk][рp]ы[тt][оo][eе][\s ]?[aа][kк]ци[oо][hн][eе][pр][hн][oо][еe][\s ][oо]бщ[eе]щ[еe][cс][tт][вb][oо]', 1, 1, 'i')
-  AND NOT REGEXP_LIKE(adr, '[mМ][oо][сc][kк][bв][aа]|MSK|МСК|moscow', 1, 1, 'i')
+  SELECT ops.*
+  FROM sh.test_operation ops
+  JOIN sh.test_uchast uch ON uch.inn = ops.inn_pol
+  WHERE REGEXP_LIKE(uch.naimen, '[З3][AА][OО]|[з3][аa][кk][рp]ы[тt][оo][eе][\s ]?[aа][kк]ци[oо][hн][eе][pр][hн][oо][еe][\s ][oо]бщ[eе]щ[еe][cс][tт][вb][oо]', 1, 1, 'i')
+  AND NOT REGEXP_LIKE(ops.bic_pol, '^0445')
 
 UPDATE sh.operations
 SET col = val
