d13bac4b535580b4516d458984135f47a6a53bd5
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-02-27 18:34:20 +0300

    done

--- db-sem6-admin/adm_lab2.sql
+++ db-sem6-admin/adm_lab2.sql
@@ -5,15 +5,10 @@
 */
 
 DROP TABLE fgup_payer;
-
-CREATE TABLE fgup_payer AS
-    SELECT * FROM sh.test_operation
-    WHERE 1 = 0;
-
 DROP PROCEDURE fgup;
 DROP TABLE logger;
-
 DROP SEQUENCE log_proc_seq;
+
 
 CREATE TABLE logger (
     ID NUMBER PRIMARY KEY,
@@ -25,35 +20,73 @@ CREATE TABLE logger (
     FLAG NUMBER NOT NULL
 );
 
+CREATE TABLE fgup_payer AS
+    SELECT * FROM sh.test_operation
+    WHERE rownum < 1;
+
+ALTER TABLE fgup_payer 
+    ADD proc_id NUMBER;
+    
+
+
 CREATE SEQUENCE log_proc_seq
     START WITH 1
     INCREMENT BY 1
     NOMAXVALUE;
 
-CREATE OR REPLACE PROCEDURE fgup(date1 IN DATE, date2 IN DATE) IS
+CREATE OR REPLACE PROCEDURE fgup(date1 IN DATE, date2 IN DATE, oper_descr IN sh.test_operation.descr%TYPE) IS
     CURSOR cur_operations IS
         SELECT * FROM sh.test_operation
-        WHERE data_op BETWEEN date1 AND date2 AND REGEXP_LIKE(upper(descr), 'ФГУП');
+        WHERE data_op BETWEEN date1 AND date2 
+            AND REGEXP_LIKE(upper(descr), 'ФГУП')
+            AND REGEXP_LIKE(upper(descr), oper_descr);
     v_cnt NUMBER := 0;
     v_st_pr DATE := SYSDATE;
+    seq_val NUMBER;
 BEGIN
-    DELETE FROM fgup_payer;
-    FOR rec IN cur_operations LOOP
-        INSERT INTO fgup_payer VALUES rec;
+    seq_val := log_proc_seq.NEXTVAL;
+   -- DELETE FROM fgup_payer;    
+    FOR i IN cur_operations LOOP
+        INSERT INTO fgup_payer(KOD,DOG,DATA_DOG,SUMMA_DOG,BIC_PL,KORCH_PL,RASCH_PL,INN_PL,BIC_POL,KORSCH_POL,RASCH_POL,INN_POL,DESCR,DATA_OP,SUMMA_OP,RULE,PRIZ,proc_id) 
+        VALUES (i.KOD,i.DOG,i.DATA_DOG,i.SUMMA_DOG,i.BIC_PL,i.KORCH_PL,i.RASCH_PL,i.INN_PL,i.BIC_POL,i.KORSCH_POL,i.RASCH_POL,i.INN_POL,i.DESCR,i.DATA_OP,i.SUMMA_OP,i.RULE,i.PRIZ,seq_val);
         v_cnt := v_cnt + 1;
     END LOOP;
 
-    INSERT INTO logger (ID, DATE_PROC_START, DATE_FROM, DATE_TO, COUNT_INSERT, DATE_PROC_END, FLAG)
-             VALUES (log_proc_seq.NEXTVAL, v_st_pr, date1, date2, v_cnt, SYSDATE, 1);
+INSERT INTO logger (ID, DATE_PROC_START, DATE_FROM, DATE_TO, COUNT_INSERT, DATE_PROC_END, FLAG)
+        VALUES (seq_val, v_st_pr, date1, date2, v_cnt, SYSDATE, 1);
+         
+
 EXCEPTION
     WHEN OTHERS THEN
         INSERT INTO logger (ID, DATE_PROC_START, DATE_FROM, DATE_TO, COUNT_INSERT, DATE_PROC_END, FLAG)
-                 VALUES (log_proc_seq.NEXTVAL, v_st_pr, date1, date2, v_cnt, SYSDATE, 0);
+                 VALUES (seq_val, v_st_pr, date1, date2, v_cnt, SYSDATE, 0);
+    RAISE;
 END;
 
-begin 
-    fgup(to_date('01.01.2001'),to_date('01.01.2010'));
-end;
+BEGIN
+    fgup(to_date('01.01.2001'),to_date('01.01.2010'), 'НДС');
+    fgup(to_date('01.01.2001'),to_date('01.01.2010'), 'УГОЛЬ');
+END;
+SELECT * FROM logger;
 
-select * from logger;
 SELECT * FROM fgup_payer;
+
+SELECT * FROM sh.test_operation
+WHERE data_op BETWEEN to_date('01.01.2001') AND ('01.01.2010') 
+    AND REGEXP_LIKE(upper(descr), 'ФГУП')
+    AND REGEXP_LIKE(upper(descr), 'НДС');
+
+SELECT * FROM sh.test_operation
+WHERE data_op BETWEEN to_date('01.01.2001') AND ('01.01.2010') 
+    AND REGEXP_LIKE(upper(descr), 'ФГУП')
+    AND REGEXP_LIKE(upper(descr), 'УГОЛЬ');
+
+-- параметр на вход для описания процедуры
+-- аппенд данных без очистки
+-- доп столбец в фгуп пэйер для указания номера процедуры
+
+
+
+ALTER TABLE fgup_payer
+    ADD CONSTRAINT fk_proc_id FOREIGN KEY (proc_id)
+    REFERENCES logger(ID);
