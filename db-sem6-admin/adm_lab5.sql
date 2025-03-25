3555c8bd556afa65c09a402b2f9a9d1fe195c5db
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 12:06:38 +0300

    Update adm_lab5.sql

--- db-sem6-admin/adm_lab5.sql
+++ db-sem6-admin/adm_lab5.sql
@@ -5,6 +5,6 @@ DBMS_SCHEDULER.ALTER_COSTS_INDEX_JOB (
     job_action       => 'BEGIN SH.ALTER_TAB_INDX('COSTS'); END;',
     job_class        => 'DBMS_JOB$',
     start_date       => TRUNC(sysdate),
-    repeat_interval  => 'FREQ=DAILY; BYMONTH=1,2,3,4; BYDAY=FRI; BYHOUR=0,4,8,12,16,20',
+    repeat_interval  => 'FREQ=HOURLY; INTERVAL=4; BYMONTH=1,2,3,4; BYDAY=FRI; BYMINUTE=0; BYSECOND=0',
     enabled          => TRUE);
 END;
