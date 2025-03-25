239445c97a096bdf80b6d1990fd6065f2de8a3fd
Sab <66420569+iceisnicehq@users.noreply.github.com>
2025-03-25 12:20:22 +0300

    Update adm_lab5.sql

--- db-sem6-admin/adm_lab5.sql
+++ db-sem6-admin/adm_lab5.sql
@@ -1,10 +1,17 @@
 BEGIN
-DBMS_SCHEDULER.ALTER_COSTS_INDEX_JOB (
+DBMS_SCHEDULER.CREATE_JOB (
     job_name         => 'ALTER_COSTS_INDEX_JOB',
     job_type         => 'PLSQL_BLOCK',
-    job_action       => 'BEGIN SH.ALTER_TAB_INDX('COSTS'); END;',
+    job_action       => 'BEGIN SH.ALTER_TAB_INDX(''COSTS''); END;',
     job_class        => 'DBMS_JOB$',
     start_date       => TRUNC(sysdate),
-    repeat_interval  => 'FREQ=HOURLY; INTERVAL=4; BYMONTH=1,2,3,4; BYDAY=FRI; BYMINUTE=0; BYSECOND=0',
+    repeat_interval  => 'FREQ=HOURLY; INTERVAL=4; BYMONTH=1,2,3,4; BYDAY=FRI',
     enabled          => TRUE);
 END;
+
+BEGIN
+DBMS_SCHEDULER.DROP_JOB(    job_name         => 'ALTER_COSTS_INDEX_JOB');
+END;
+
+select * from DBA_SCHEDULER_JOBS order by job_name;
+-- select * from COSTS;
